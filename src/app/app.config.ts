a89091253108a64d6311b9f843dbcc2c5efb2232
Karla Tovar <ktovar@jacidi.com>
2024-08-08 20:23:06 -0400

    feat: integration services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,15 +3,18 @@ import {
   importProvidersFrom,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthService } from './core/services/auth_service';
+import { provideNgxStripe } from 'ngx-stripe';
+import { CartService } from './core/services/cart_service';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { PurchaseService } from './core/services/purchase_service';
 
 const I18N_CONFIG = {
@@ -27,12 +30,14 @@ export function HttpLoaderFactory(http: HttpClient) {
 }
 export const appConfig: ApplicationConfig = {
   providers: [
-    AuthService,
-    PurchaseService,
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    AuthService,
+    CartService,
+    PurchaseService,
+    provideNgxStripe(),
     importProvidersFrom(TranslateModule.forRoot(I18N_CONFIG)),
     importProvidersFrom([BrowserAnimationsModule]),
   ],
